4e0305d462e78f6ceda79fd17b8bad0ae095cf43
rezarajib <rajibreza64@gmail.com>
2023-03-13 01:26:35 +0600

    first commit

---
+++ fundamentals.js
@@ -0,0 +1,38 @@
+// 1. How to declare a variable sing let and const   
+
+//  fixed variable use const variable 
+const fatherName = "azajar bapari";
+//  any variable change for use let variable use 
+let season = "rainy";
+let season2 = "winter";
+
+//  2. use condition use third method 6 basic condition > , <, ===, !==,<=,>=
+//  multiple conditions: && , || 
+
+
+//  number three array 
+//  array of index 
+//  length,push, 
+
+//  loop for use 4 
+
+//  number five function 
+
+//  number six object 
+
+const student = {
+    name : 'rajib reza',
+    age : 22,
+    story : 'aim in life',
+    assinment : ['first','two','three']
+}
+
+const myVariable = 'age';
+// 3 way to   access property by name
+//  direct by property 
+console.log(student.age);
+
+//  access via property name string
+console.log(student['age'])
+//  access via property name in a variable
+console.log(student[myVariable]); 
